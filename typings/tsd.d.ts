68b97e2ed20e5ca7fae4bae80a4c7a25dd2f3991
davetayls <dave@the-taylors.org>
2015-01-22 11:11:01 +0000

    added tsd definitions

---
+++ typings/tsd.d.ts
@@ -0,0 +1,10 @@
+
+/// <reference path="backbone/backbone.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="fastclick/fastclick.d.ts" />
+/// <reference path="marionette/marionette.d.ts" />
+/// <reference path="i18next/i18next.d.ts" />
+/// <reference path="handlebars/handlebars.d.ts" />
+/// <reference path="modernizr/modernizr.d.ts" />
+/// <reference path="showdown/showdown.d.ts" />
