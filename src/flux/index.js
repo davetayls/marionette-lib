6c51fe31519e1342dabae8ab3c23c969d7c2477e
davetayls <dave@the-taylors.org>
2015-03-05 16:28:03 +0000

    export the flux items

---
+++ src/flux/index.js
@@ -0,0 +1,5 @@
+exports.actions = require('./actions');
+exports.Dispatcher = require('./Dispatcher');
+exports.interfaces = require('./interfaces');
+exports.Store = require('Store');
+//# sourceMappingURL=index.js.map
---
+++ src/flux/index.ts
@@ -0,0 +1,5 @@
+
+export import actions = require('./actions');
+export import Dispatcher = require('./Dispatcher');
+export import interfaces = require('./interfaces');
+export import Store = require('Store');
