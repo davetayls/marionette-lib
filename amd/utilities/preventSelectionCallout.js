a8a158610d83c72814a8f69adbca2f766ad373d1
davetayls <dave@the-taylors.org>
2015-03-26 13:37:42 +0000

    added new files to amd

---
+++ amd/utilities/preventSelectionCallout.js
@@ -0,0 +1,8 @@
+define(function (require, exports, module) {function preventGlobally() {
+    var style = document.documentElement.style;
+    style.webkitTouchCallout = "none";
+    style.webkitUserSelect = "none";
+}
+exports.preventGlobally = preventGlobally;
+//# sourceMappingURL=preventSelectionCallout.js.map
+});
