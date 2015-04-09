9ef825204bf84ae6142f27322950d0f26fd9609d
davetayls <dave@the-taylors.org>
2015-04-09 21:56:17 +0100

    added animate functions directly to animated region:

--- src/components/AnimatedRegion/AnimatedRegion.ts
+++ src/components/AnimatedRegion/AnimatedRegion.ts
@@ -1,8 +1,32 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
+import _ = require('underscore');
 import Marionette = require('backbone.marionette');
 
+export interface IAnimatableView extends Backbone.View<Backbone.Model> {
+  animateOut(cb:()=>void):void;
+}
+
 export class AnimatedRegion extends Marionette.Region {
-  currentView:Backbone.View<Backbone.Model>;
-  _nextView:Backbone.View<Backbone.Model>;
+  currentView:IAnimatableView;
+  _nextView:IAnimatableView;
+
+  animateOut(cb:()=>void) {
+    if (this.currentView && this.currentView.animateOut) {
+      console.log('animating out', this.currentView, this.el);
+      this.currentView.animateOut(cb);
+    } else if (_.isFunction(cb)) {
+      cb();
+    }
+  }
+
+  animateEmpty(cb:()=>void) {
+    this.animateOut(() => {
+      this.empty();
+      if (_.isFunction(cb)) {
+        cb();
+      }
+    });
+  }
+
 }
