28879bc0293cec11189df34cda6ae1664d3887c6
davetayls <dave@the-taylors.org>
2015-04-14 22:00:30 +0100

    add back modernizr old in amd

---
+++ amd/modernizr/old.js
@@ -0,0 +1,19 @@
+/// <reference path="../../typings/modernizr/modernizr.d.ts" />
+Modernizr.addTest("old", function () {
+    var v = function (ua) {
+        var match = ua.match(/Android\s([0-9\.]*)/);
+        if (match) {
+            return match[1];
+        }
+        else {
+            return '';
+        }
+    }(navigator.userAgent);
+    if (v) {
+        return parseFloat(v) < 4.3;
+    }
+    else {
+        return false;
+    }
+});
+//# sourceMappingURL=old.js.map
